f12a81efa032ce702b0cfa1ad0d806a669447447
puiusabin <puiumihaisabin@gmail.com>
2026-03-29 22:58:43 +0300

    add validation schema and remove cors middleware

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const Note = require("./models/note");
 const app = express();
 
 app.use(express.static("dist"));
-app.use(cors());
 app.use(express.json());
 
 let notes = [
@@ -38,14 +37,17 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -62,19 +64,62 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+  Note.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  const { content, important } = request.body;
+
+  Note.findById(id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
-
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
--- models/note.js
+++ models/note.js
@@ -16,7 +16,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    mingLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 
